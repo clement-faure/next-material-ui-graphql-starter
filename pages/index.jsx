85db87341ecb361dae9766360e166acfcc15ba46
Clément Faure <dev@clement-faure.fr>
2020-10-10 17:34:48 +0200

    Update dependencies

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Head from 'next/head';
 import getConfig from 'next/config';
 
-import { i18n, withTranslation } from '~/lib/i18n';
+import { Button } from '@material-ui/core';
+
+import { useTranslation } from '~/lib/i18n';
 
 const {
   publicRuntimeConfig: { appName },
 } = getConfig();
 
-const Homepage = ({ t }) => (
-  <>
-    <Head>
-      <title>{`${appName} - ${t('index.head_title')}`}</title>
-    </Head>
-    <div className="padding-50">
-      <button>{t('change-locale')}</button>
-    </div>
-  </>
-);
+const Homepage = () => {
+  const { t, i18n } = useTranslation('common');
+
+  const onLanguageChange = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'fr' : 'en');
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{`${appName} - ${t('index.head_title')}`}</title>
+      </Head>
+      <div className="padding-50">
+        <Button onClick={onLanguageChange}>{t('change-locale')}</Button>
+      </div>
+    </>
+  );
+};
 
 Homepage.getInitialProps = async () => ({
   namespacesRequired: ['common'],
 });
 
-Homepage.propTypes = {
-  t: PropTypes.func.isRequired,
-};
-
-export default withTranslation('common')(Homepage);
+export default Homepage;
